e74996a5f1e09bbd71d477a7e41c8ce4e1c47a85
Asif Imtial <aimtial@gmail.com>
2021-06-10 18:21:05 +0600

    css ordering fixed

--- soccerboard/src/index.js
+++ soccerboard/src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
-import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
   <BrowserRouter>
